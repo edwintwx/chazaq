27c7a2e1a764fc393c965daefd07bdb4ca83e2c3
Edwin Ting <edwintwx@gmail.com>
2019-10-11 22:40:52 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,22 @@ user2.save!
 user3 = User.new(first_name: 'Kennie', last_name: 'Ting', email: 'kennie@gmail.com', password: 'password', city: 'Singapore', country: 'Singapore' )
 user3.save!
 
+text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda in obcaecati esse, aliquam illo soluta itaque aperiam voluptate id. Impedit natus tenetur quisquam ipsa ab ex, praesentium saepe nobis?'
 
-p1 = Post.new(content: 'This is my first content', request: true, user_id: 1)
+p1 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 1)
 p1.save!
 
-p2 = Post.new(content: 'This is my 2nd content', request: true, user_id: 2)
+p2 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 2)
 p2.save!
 
-p3 = Post.new(content: 'This is my 3rd content', request: true, user_id: 3)
+p3 = Post.new(content: text, request: true, isPrivate: true, category: 'request', user_id: 3)
 p3.save!
 
-p4 = Post.new(content: 'This is a testimonial', testimonial: true, user_id: 1)
+p4 = Post.new(content: text, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
 p4.save!
 
-p5 = Post.new(content: 'This is another testimonial content', testimonial: true, user_id: 1)
+p5 = Post.new(content: text, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
 p5.save!
 
-p6 = Post.new(content: 'This is yet another testimonial content piece', request: true, user_id: 3)
+p6 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 3)
 p6.save!
