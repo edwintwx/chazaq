fecc113752e6990d82962bf993a0861ddc7bbac3
Edwin Ting <edwintwx@gmail.com>
2019-10-27 21:47:28 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+User.destroy_all
+Post.destroy_all
+PostPhoto.destroy_all
+
 user1 = User.new(first_name: 'Edwin', last_name: 'Ting', email: 'edwintwx@gmail.com', password: 'password', city: 'Singapore', country: 'Singapore' )
 user1.save!
 
@@ -7,22 +11,26 @@ user2.save!
 user3 = User.new(first_name: 'Kennie', last_name: 'Ting', email: 'kennie@gmail.com', password: 'password', city: 'Singapore', country: 'Singapore' )
 user3.save!
 
-text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda in obcaecati esse, aliquam illo soluta itaque aperiam voluptate id. Impedit natus tenetur quisquam ipsa ab ex, praesentium saepe nobis?'
-
-p1 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 1)
+text1 = 'These things I have spoken to you, that in Me you may have peace. In the world you will have tribulation; but be of good cheer, I have overcome the world.'
+p1 = Post.new(content: text1, request: true, isPrivate: false, category: 'request', user_id: 1)
 p1.save!
 
-p2 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 2)
+text2 = 'Do you not know that you are the temple of God and that the Spirit of God dwells in you?'
+p2 = Post.new(content: text2, request: true, isPrivate: false, category: 'request', user_id: 2)
 p2.save!
 
-p3 = Post.new(content: text, request: true, isPrivate: true, category: 'request', user_id: 3)
+text3 = 'But you, beloved, building yourselves up on your most holy faith, praying in the Holy Spirit, keep yourselves in the love of God, looking for the mercy of our Lord Jesus Christ unto eternal life.'
+p3 = Post.new(content: text3, request: true, isPrivate: true, category: 'request', user_id: 3)
 p3.save!
 
-p4 = Post.new(content: text, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
+text4 = 'And Jesus said to them, “I am the bread of life. He who comes to Me shall never hunger, and he who believes in Me shall never thirst.'
+p4 = Post.new(content: text4, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
 p4.save!
 
-p5 = Post.new(content: text, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
+text5 = 'And without controversy great is the mystery of godliness: God was manifested in the flesh, Justified in the Spirit, Seen by angels, Preached among the Gentiles, Believed on in the world, Received up in glory.'
+p5 = Post.new(content: text5, testimonial: true, isPrivate: false, category: 'testimonial', user_id: 1)
 p5.save!
 
-p6 = Post.new(content: text, request: true, isPrivate: false, category: 'request', user_id: 3)
+text6 = 'Blessed is the man who trusts in the LORD, And whose hope is the LORD.'
+p6 = Post.new(content: text6, request: true, isPrivate: false, category: 'request', user_id: 3)
 p6.save!
